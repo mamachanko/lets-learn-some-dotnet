4ba0d19d4bae81f875cd7d50b5a6d46bc637e64f
mamachanko <bfahc2cbv@relay.firefox.com>
2020-11-26 09:51:37 +0100

    Iterate on basics demo

--- demo-basics.sh
+++ demo-basics.sh
@@ -26,7 +26,99 @@ clear
 # Intro #
 #########
 
-pe "echo hello"
+pe "# What is it ❓"
+pe "# What you can do ❗"
+pe "# How you do it ❓❗"
+pe ""
+pe "# 🤷"
+pei "dotnet --help"
+
+# the idea:
+# identify animals by feature
+# read a feature from stdin
+# write matching animals to stdout
+
+# the cli part in C#
+# the identification part in F#
+
+dotnet new solution --name AnimalCompendium
+tree
+
+dotnet new console --name Cli
+dotnet sln AnimalCompendium.sln add Cli
+tree
+
+dotnet run --project Cli
+
+dotnet new classlib --name Lib --language "F#"
+dotnet sln AnimalCompendium.sln add Lib
+dotnet add Cli reference Lib
+tree
+
+
+rider AnimalCompendium.sln
+
+# using System;
+# using System.Linq;
+# using static AnimalCompendiumLib.AnimalCompendium;
+# 
+# namespace AnimalCompendiumCli
+# {
+#     class Program
+#     {
+#         static void Main(string[] args)
+#         {
+#             var feature = Console.ReadLine();
+#             var animals = Identify(feature);
+# 
+#             if (animals.Any())
+#             {
+#                 foreach (var animal in animals)
+#                 {
+#                     Console.WriteLine(animal);
+#                 }
+#             }
+#             else
+#             {
+#                 Console.Error.WriteLine("Could not identify any animals.");
+#             }
+#         }
+#     }
+# }
+
+# namespace AnimalCompendiumLib
+# 
+# module AnimalCompendium =
+# 
+#     let dog = "🐶"
+#     let cat = "🐱"
+#     let octopus = "🐙"
+# 
+#     let Identify (feature: string): List<string> =
+#         match feature with
+#         | "walks" -> [ cat; dog ]
+#         | "barks" -> [ dog ]
+#         | "meows" -> [ cat ]
+#         | "swims" -> [ octopus ]
+#         | _ -> []
+# 
+
+
+echo walks | dotnet run --project Cli
+
+dotnet publish --self-contained --runtime osx-x64 --output ./out
+
+echo walks | ./out/Cli
+echo barks | ./out/Cli
+echo swims | ./out/Cli
+
+# Demo
+# * dotnet cli
+# * create an c# console app
+# * IDEs
+# * create an f# library
+# * run it
+# * publish it
 
 ############
 # The end. #
